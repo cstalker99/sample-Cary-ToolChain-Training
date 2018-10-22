56ca9a3aa56e277bbdc48273360eab971c0ee190
Cary Stalker <c_stalker@hotmail.com>
2018-10-22 17:48:51 +0000

    reverted

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -39,7 +39,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hello, Cary was here!');
+            res.body.should.eql('Hello, Scott!');
 
             done();
           });
